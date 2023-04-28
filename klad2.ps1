5d8a8c3d4e6e7f22f6c4a3e03478ccb649ae8631
Michiel Hamers (Hammertime) <michielhamers@outlook.com>
2023-04-28 18:38:56 +0200

    Update klad2.ps1

--- klad2.ps1
+++ klad2.ps1
@@ -10,6 +10,44 @@ $a.Screens | % {
 }
 # | Select -ExpandProperty Type
 
+<<<<<<< Updated upstream
 $a.screens | where {$_.screen -eq 5}
 
 
+=======
+Function Show-SegmentDisplay {
+    param(
+        [Parameter(Mandatory=$true)][string]$InputString
+    )
+    $SegmentDisplay = @{
+        '0' = '1111110'
+        '1' = '0110000'
+        '2' = '1101101'
+        '3' = '1111001'
+        '4' = '0110011'
+        '5' = '1011011'
+        '6' = '1011111'
+        '7' = '1110000'
+        '8' = '1111111'
+        '9' = '1111011'
+        'A' = '1110111'
+        'B' = '0011111'
+        'C' = '1001110'
+        'D' = '0111101'
+        'E' = '1001111'
+        'F' = '1000111'
+    }
+    ForEach ($Char in $InputString.ToCharArray()) {
+        $DisplayString = ""
+        ForEach ($Segment in $SegmentDisplay[$Char].ToCharArray()) {
+            If ($Segment -eq "1") {
+                $DisplayString += "|"
+            } Else {
+                $DisplayString += " "
+            }
+        }
+        Write-Host $DisplayString
+    }
+}
+Show-SegmentDisplay "1234"
+>>>>>>> Stashed changes
